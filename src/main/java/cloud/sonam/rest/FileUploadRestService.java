1d945b7f1ea70436a51c97551f4100955430db55
Sonam Samdupkhangsar <me@sonam.email>
2021-05-12 12:36:20 -0600

    add fileuploadrestservice

---
+++ src/main/java/cloud/sonam/rest/FileUploadRestService.java
@@ -0,0 +1,29 @@
+package cloud.sonam.rest;
+
+import cloud.sonam.db.service.FileUploadService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/file")
+public class FileUploadRestService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FileUploadRestService.class);
+
+    @Autowired
+    private FileUploadService fileUploadService;
+
+    @PostMapping(value="/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Mono<String> upload( @RequestPart("file") Mono<FilePart> filePartFlux) {
+        LOG.info("upload got called");
+       return fileUploadService.save(filePartFlux);
+    }
+}
